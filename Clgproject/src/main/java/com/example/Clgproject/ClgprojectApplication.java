c9f392c09e6d9c6da84280cabd878f0d3db6b77b
rpjtesting <rpjtesting@gmail.com>
2022-09-18 15:32:44 +0530

    First commit of MiniProj
    uploaded all files

---
+++ Clgproject/src/main/java/com/example/Clgproject/ClgprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.Clgproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClgprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClgprojectApplication.class, args);
+	}
+
+}
+	
---
+++ Clgproject/src/test/java/com/example/Clgproject/ClgprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clgproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClgprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
